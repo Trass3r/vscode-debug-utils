fbbfef77eee67ec6e4571d07ff40db9363091af1
Andreas Hollandt <trass3r@gmail.com>
2020-09-21 22:10:03 +0200

    setup vscode extension

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vs from 'vscode';
+
+// this method is called when your extension is activated
+export function activate(context: vs.ExtensionContext) {
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
